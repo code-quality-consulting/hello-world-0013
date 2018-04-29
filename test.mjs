a68ba17c4254f522dd8ec37dbd91848edbf32586
Zach Overhulser <zoverhulser@gmail.com>
2018-04-29 18:38:43 -0500

    Tests greeting in Russian.

--- test.mjs
+++ test.mjs
@@ -8,7 +8,8 @@ import {makeGreeter} from "./index";
 const greetings = {
     english: makeGreeter("English"),
     spanish: makeGreeter("Spanish"),
-    german: makeGreeter("German")
+    german: makeGreeter("German"),
+    russian: makeGreeter("Russian")
 };
 
 assert.strictEqual(greetings.english(), "Hello world!");
@@ -17,4 +18,5 @@ assert.strictEqual(greetings.spanish(), "¡Hola mundo!");
 assert.strictEqual(greetings.spanish("Alejandro"), "¡Hola Alejandro!");
 assert.strictEqual(greetings.german(), "Hallo Welt!");
 assert.strictEqual(greetings.german("Gunter"), "Hallo Gunter!");
+assert.strictEqual(greetings.russian(), "Привет мир");
 console.log("Your test has passed!");
