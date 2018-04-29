da3559f809293f50f6c216529ac4163618845e70
Zach Overhulser <zoverhulser@gmail.com>
2018-04-29 00:04:45 -0500

    Greets user by name in Spanish.

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 */
 module.exports = function makeGreeting(name = "world", language = "English") {
     if (language === "Spanish") {
+        if (name !== "") {
+            return "¡Hola " + name + "!";
+        }
         return "¡Hola mundo!";
     }
     return "Hello " + name + "!";
