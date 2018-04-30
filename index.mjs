39722c21ee22a088e01c48a4017bacaa4dc5967f
Zach Overhulser <zoverhulser@gmail.com>
2018-04-29 22:26:58 -0500

    Greets users in Chinese.

--- index.mjs
+++ index.mjs
@@ -6,17 +6,24 @@ const hello = {
     "English": "Hello",
     "Spanish": "¡Hola",
     "German": "Hallo",
-    "Russian": "Привет"
+    "Russian": "Привет",
+    "Chinese": "你好"
 };
 
 const world = {
     "English": "world",
     "Spanish": "mundo",
     "German": "Welt",
-    "Russian": "мир"
+    "Russian": "мир",
+    "Chinese": "世界"
 };
 
 export function makeGreeter(language = "English") {
+    if (language === "Chinese") {
+        return function (name = world[language]) {
+            return hello[language] + " " + name;
+        }
+    }
     return function (name = world[language]) {
         return hello[language] + " " + name + "!";
     };
